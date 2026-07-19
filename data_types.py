0b97e41f151d15b52c5e70a178a14fcef5872e83
sakethghanta14-debug <sakethghanta14@gmail.com>
2026-07-19 13:58:44 -0700

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,12 @@
+a=123
+# integer is a combination of postive, negtive numbers , and zero that are whole
+print(type(a))
+b=1.23
+#float is a decimal number
+print(type (b))
+c=True
+# boolean is some thing that is true or false.
+print(type (c))
+d="hello my name is..."
+#string is one or more characters together and string string need to be written in between the quotation marks quotation marks are ("")
+print(type (d)) 
