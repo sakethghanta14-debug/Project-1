f33c791e2a910dbda899b4a3951d1ffeb2c7f7b7
sakethghanta14-debug <sakethghanta14@gmail.com>
2026-07-19 13:46:11 -0700

    Create string_operatopns.py

---
+++ string_operatopns.py
@@ -0,0 +1,9 @@
+last_name="Ghanta"
+name="Saketh"
+print(name[0]) #indexing
+print(name[0:5]) #sliceing
+print(len(name)) #length of sting
+print(name+" "+last_name) #concatation
+print(name.lower())#lowercase
+print(last_name.upper())#uppercase
+print(name[-1])#negitve indexing starts from -1 or the last letter
