a3ea5da6c474c8cc81983c120f4f70005d126708
sakethghanta14-debug <sakethghanta14@gmail.com>
2026-07-19 13:57:32 -0700

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,11 @@
+#A vairiable is a container that stores data
+#1- Before asking question we need to create the variable
+#2- After creating wariable we need to mention the datatype
+#3- Taking the input
+#4- Write what you want to ask 
+name=str(input("What is your name"))
+print("Hello",name)
+age=int(input("How old are you"))
+email = input("What is your gmail ")
+getting_to_know = input("What games do you like to play ")
+print("cool games", name)
