d0d02626fee1b7b159853e598a3c3fbf9ee0051c
sakethghanta14-debug <sakethghanta14@gmail.com>
2026-07-26 16:46:35 -0700

    Create assignment_operators.py

---
+++ assignment_operators.py
@@ -0,0 +1,8 @@
+gamers = 1000
+youtubers = 250
+
+print(gamers < youtubers) # false - 1000 isn't smaller than 250
+print(gamers > youtubers) # true - 1000 is greater than 250
+print(gamers == youtubers) #false - 1000 isn't equal to 250
+print(gamers <= youtubers) #false - 1000 isn't smaller or equal to 250
+print(gamers >= youtubers) #true - 1000 is larger or equal to 250
