7a49cb27c930e5643a3d055f3e8678b02a26ef3a
sakethghanta14-debug <sakethghanta14@gmail.com>
2026-08-16 15:31:55 -0700

    Create days.py

---
+++ days.py
@@ -0,0 +1,16 @@
+day = str(input("What day is it?"))
+
+if day == "Sunday":
+    print("Today is Sunday")
+elif day == "Monday":
+    print("Today is Monday ")
+elif day == "Tuesday":
+    print("Today is monday")
+elif day == "Wenesday":
+    print("Today is monday")
+elif day == "Thursday":
+    print("Today is ")
+elif day == "Friday":
+    print("Today is Friday")
+elif day == "Saturday":
+    print("Today is Saturday")
