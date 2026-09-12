a426f513b773677dcfe19aeff5579d6a5e8c842f
sakethghanta14-debug <sakethghanta14@gmail.com>
2026-09-11 17:42:56 -0700

    Create mean.py

---
+++ mean.py
@@ -0,0 +1,17 @@
+mean1 = 38
+wrong_num = 36
+correct_nnum = 56
+total_num = 40
+#sum of 40 numbers
+sum = mean1*total_num
+print("the sum of 40 numbers is " ,sum)
+
+
+#correct sum of numbers
+num2=sum-((wrong_num))-((correct_nnum))
+print("sum-((wrong_num))-((correct_num)):" ,num2)
+
+
+#the correct mean
+mean2 =num2 / total_num
+print("The correct mean is" ,mean2)
