ffe030a2331f7307fade132b41ff4c27220326a1
sakethghanta14-debug <sakethghanta14@gmail.com>
2026-09-11 17:44:30 -0700

    Create identity_mebership_operators.py

---
+++ identity_mebership_operators.py
@@ -0,0 +1,11 @@
+#Identity operator : uses is and is not ti check if somethig is true and false
+a = 9
+b = 90
+print(a is b)
+print(a is not b)
+
+#membership operator: uses not in and in and checkes if a string or anything else is true and false by letter or anything else
+c = "pizza"
+print("c" not in c)
+print("p" in c)
+print("z" in c)
